341f74f6a73b09b951a5973b7d62ae7b02f63429
i3bdolah <xbdullahx@gmail.com>
2024-07-18 23:36:10 +0300

    Part 2, Icons, and Default Buttons

--- WindowsFormsApp1/Form3.Designer.cs
+++ WindowsFormsApp1/Form3.Designer.cs
@@ -31,13 +31,15 @@
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(293, 89);
+            this.button1.Location = new System.Drawing.Point(246, 49);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(168, 36);
+            this.button1.Size = new System.Drawing.Size(279, 36);
             this.button1.TabIndex = 0;
             this.button1.Text = "Show Message";
             this.button1.UseVisualStyleBackColor = true;
@@ -45,9 +47,9 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(293, 173);
+            this.button2.Location = new System.Drawing.Point(246, 123);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(168, 36);
+            this.button2.Size = new System.Drawing.Size(279, 36);
             this.button2.TabIndex = 1;
             this.button2.Text = "Show Message With Title";
             this.button2.UseVisualStyleBackColor = true;
@@ -55,19 +57,41 @@
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(293, 255);
+            this.button3.Location = new System.Drawing.Point(246, 197);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(168, 36);
+            this.button3.Size = new System.Drawing.Size(279, 36);
             this.button3.TabIndex = 2;
             this.button3.Text = "Show Message OK/CANCEL";
             this.button3.UseVisualStyleBackColor = true;
             this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(246, 271);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(279, 36);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "Show Message OK/CANCEL and Icon";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(246, 346);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(279, 36);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "Show Message OK/CANCEL and Icon and Default btn";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
             // 
             // Form3
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
@@ -82,5 +106,7 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
     }
 }
--- WindowsFormsApp1/Form3.cs
+++ WindowsFormsApp1/Form3.cs
@@ -29,7 +29,32 @@ namespace WindowsFormsApp1
 
         private void button3_Click(object sender, EventArgs e)
         {
-            if (MessageBox.Show("Hi", "Welcome", MessageBoxButtons.OKCancel, MessageBoxIcon.Error) == DialogResult.OK)
+            if (MessageBox.Show("Hi", "Welcome", MessageBoxButtons.OKCancel) == DialogResult.OK)
+            {
+                MessageBox.Show("You've Pressed OK.");
+            }
+            else
+            {
+                MessageBox.Show("You've Pressed Cancel.");
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Hi", "Welcome", MessageBoxButtons.OKCancel, MessageBoxIcon.Question) == DialogResult.OK)
+            {
+                MessageBox.Show("You've Pressed OK.");
+            }
+            else
+            {
+                MessageBox.Show("You've Pressed Cancel.");
+            }
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Hi", "Welcome", MessageBoxButtons.OKCancel, MessageBoxIcon.Question, MessageBoxDefaultButton.Button2) == DialogResult.OK)
             {
                 MessageBox.Show("You've Pressed OK.");
             }
