7d78355330b17cd3c4de9c1489b97b8c383ce558
i3bdolah <xbdullahx@gmail.com>
2024-07-26 23:02:29 +0300

    Using sender parameter

--- PictureBoxExercise/Form1.cs
+++ PictureBoxExercise/Form1.cs
@@ -18,28 +18,28 @@ namespace PictureBoxExercise
             InitializeComponent();
         }
 
-        private void radioButton2_CheckedChanged(object sender, EventArgs e)
-        {
-
-        }
-
         private void radioButton1_CheckedChanged(object sender, EventArgs e)
         {
+            label1.Text = ((RadioButton)sender).Text;
             pictureBox1.Image = Resources.Girl;
         }
 
         private void radioButton2_CheckedChanged_1(object sender, EventArgs e)
         {
+            //label1.Text = radioButton2.Text;
+            label1.Text = ((RadioButton) sender).Text;
             pictureBox1.Image = Resources.Boy;
         }
 
         private void radioButton3_CheckedChanged(object sender, EventArgs e)
         {
+            label1.Text = ((RadioButton)sender).Text;
             pictureBox1.Image = Resources.Book;
         }
 
         private void radioButton4_CheckedChanged(object sender, EventArgs e)
         {
+            label1.Text = ((RadioButton)sender).Text;
             pictureBox1.Image = Image.FromFile(@"C:\Users\Abdullah\Desktop\Images\Images\Pen.png");
         }
     }
