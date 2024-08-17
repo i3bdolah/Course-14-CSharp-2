1a4c02b490f367ae07007e2e5d8c4b431227f6c4
i3bdolah <xbdullahx@gmail.com>
2024-08-17 23:21:01 +0300

    NotifyIcon is Done

---
+++ NotifyIcon/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NotifyIcon
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            notifyIcon1.Icon = SystemIcons.Application;
+            notifyIcon1.BalloonTipIcon = ToolTipIcon.Error;
+            notifyIcon1.BalloonTipTitle = "مرحبا";
+            notifyIcon1.BalloonTipText = "أعتز ولا تغتر";
+            notifyIcon1.ShowBalloonTip(1000);
+        }
+
+        private void notifyIcon1_BalloonTipClicked(object sender, EventArgs e)
+        {
+            MessageBox.Show("User Clicked : )");
+        }
+
+        private void notifyIcon1_BalloonTipClosed(object sender, EventArgs e)
+        {
+            MessageBox.Show("User Closed : )");
+        }
+    }
+}
