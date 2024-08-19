b65271b256d73e40f2f8473167c1c81531262775
i3bdolah <xbdullahx@gmail.com>
2024-08-19 22:20:59 +0300

    TreeView and ImageList is Done

---
+++ TreeViewAndImageList/Form1.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace TreeViewAndImageList
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            System.Windows.Forms.TreeNode treeNode1 = new System.Windows.Forms.TreeNode("Saudi", 5, 5);
+            System.Windows.Forms.TreeNode treeNode2 = new System.Windows.Forms.TreeNode("Asia", 1, 1, new System.Windows.Forms.TreeNode[] {
+            treeNode1});
+            System.Windows.Forms.TreeNode treeNode3 = new System.Windows.Forms.TreeNode("Africa", 2, 2);
+            System.Windows.Forms.TreeNode treeNode4 = new System.Windows.Forms.TreeNode("Europe", 3, 3);
+            System.Windows.Forms.TreeNode treeNode5 = new System.Windows.Forms.TreeNode("America", 4, 4);
+            System.Windows.Forms.TreeNode treeNode6 = new System.Windows.Forms.TreeNode("World", 0, 0, new System.Windows.Forms.TreeNode[] {
+            treeNode2,
+            treeNode3,
+            treeNode4,
+            treeNode5});
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.SuspendLayout();
+            // 
+            // imageList1
+            // 
+            this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList1.Images.SetKeyName(0, "world.png");
+            this.imageList1.Images.SetKeyName(1, "asia.png");
+            this.imageList1.Images.SetKeyName(2, "africa.png");
+            this.imageList1.Images.SetKeyName(3, "europe.png");
+            this.imageList1.Images.SetKeyName(4, "america.png");
+            this.imageList1.Images.SetKeyName(5, "saudi.png");
+            // 
+            // treeView1
+            // 
+            this.treeView1.CheckBoxes = true;
+            this.treeView1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.treeView1.ImageIndex = 0;
+            this.treeView1.ImageList = this.imageList1;
+            this.treeView1.Location = new System.Drawing.Point(222, 31);
+            this.treeView1.Name = "treeView1";
+            treeNode1.ImageIndex = 5;
+            treeNode1.Name = "saudi";
+            treeNode1.SelectedImageIndex = 5;
+            treeNode1.Text = "Saudi";
+            treeNode2.ImageIndex = 1;
+            treeNode2.Name = "asia";
+            treeNode2.SelectedImageIndex = 1;
+            treeNode2.Text = "Asia";
+            treeNode3.ImageIndex = 2;
+            treeNode3.Name = "africa";
+            treeNode3.SelectedImageIndex = 2;
+            treeNode3.Text = "Africa";
+            treeNode4.ImageIndex = 3;
+            treeNode4.Name = "europe";
+            treeNode4.SelectedImageIndex = 3;
+            treeNode4.Text = "Europe";
+            treeNode5.ImageIndex = 4;
+            treeNode5.Name = "america";
+            treeNode5.SelectedImageIndex = 4;
+            treeNode5.Text = "America";
+            treeNode6.ImageIndex = 0;
+            treeNode6.Name = "Node0";
+            treeNode6.SelectedImageIndex = 0;
+            treeNode6.Text = "World";
+            this.treeView1.Nodes.AddRange(new System.Windows.Forms.TreeNode[] {
+            treeNode6});
+            this.treeView1.SelectedImageIndex = 0;
+            this.treeView1.Size = new System.Drawing.Size(329, 407);
+            this.treeView1.TabIndex = 0;
+            this.treeView1.AfterCheck += new System.Windows.Forms.TreeViewEventHandler(this.treeView1_AfterCheck);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.treeView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.TreeView treeView1;
+    }
+}
+
---
+++ TreeViewAndImageList/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TreeViewAndImageList
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void treeView1_AfterCheck(object sender, TreeViewEventArgs e)
+        {
+            this.CheckTreeViewNode(e.Node, e.Node.Checked);
+        }
+
+        private void CheckTreeViewNode(TreeNode node, bool isChecked)
+        {
+            foreach (TreeNode childNode in node.Nodes)
+            {
+                childNode.Checked = isChecked;
+
+                if (childNode.Nodes.Count > 0)
+                {
+                    this.CheckTreeViewNode(childNode, isChecked);
+                }
+            }
+        }
+    }
+}
